59b99a431974cbd2cd59999b5786f6ac2b6be913
Riad <riadk910@gmail.com>
2025-10-12 17:46:26 +0600

    Bankers algorithm for deadlock

---
+++ bankers.py
@@ -0,0 +1,39 @@
+n = int(input("Enter number of processes: "))
+m = int(input("Enter number of resource types: "))
+
+print("\nEnter Allocation Matrix:")
+allocation = []
+for i in range(n):
+    allocation.append(list(map(int, input(f"Process {i+1}: ").split())))
+
+print("\nEnter Maximum Matrix:")
+maximum = []
+for i in range(n):
+    maximum.append(list(map(int, input(f"Process {i+1}: ").split())))
+
+available = list(map(int, input("\nEnter Available Resources: ").split()))
+
+need = []
+for i in range(n):
+    need.append([maximum[i][j] - allocation[i][j] for j in range(m)])
+
+finished = [0] * n
+safe_sequence = []
+
+while len(safe_sequence) < n:
+    allocated = False
+    for i in range(n):
+        if finished[i] == 0 and all(need[i][j] <= available[j] for j in range(m)):
+            for j in range(m):
+                available[j] += allocation[i][j]
+            safe_sequence.append(i + 1)
+            finished[i] = 1
+            allocated = True
+    if not allocated:
+        break
+
+if len(safe_sequence) == n:
+    print("\nSystem is in a SAFE state.")
+    print("Safe Sequence:", " -> ".join(map(str, safe_sequence)))
+else:
+    print("\nSystem is in an UNSAFE state. Deadlock may occur.")
