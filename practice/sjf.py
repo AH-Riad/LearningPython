538b04d9d35daa5ccac47d94673b02fac481403b
Riad <riadk910@gmail.com>
2025-11-14 20:27:02 +0600

    Practicing sjf

---
+++ practice/sjf.py
@@ -0,0 +1,51 @@
+n = int(input("Enter number of processes:"))
+
+pid = []
+at = []
+bt = []
+
+ct = [0]*n
+tat = [0]*n
+wt = [0]*n
+completed = [0]*n
+
+for i in range(n):
+    print(f"\nProcess {i+1} details:")
+    pid.append(i+1)
+    at.append(int(input("Enter arrival time:")))
+    bt.append(int(input("Enter burst time time:")))
+    
+time = 0
+done = 0
+
+while done<n:
+    idx = -1
+    min_bt = 999999
+    
+    for i in range(n):
+        if at[i]<=time and completed[i]==0:
+            if bt[i]<min_bt:
+                min_bt = bt[i]
+                idx = i
+                
+    if idx == -1:
+        time +=1
+        continue
+    
+    time+=bt[idx]
+    ct[idx] = time
+    done +=1
+    completed[idx] =1
+    
+    
+for i in range(n):
+    tat[i] = ct[i] - at[i]
+    wt [i] = tat[i] - bt[i]
+    
+    
+    
+print("\nPID\tAT\tBT\tCT\tTAT\tWt\t")
+
+for i in range (n):
+    print(f"{pid[i]}\t{at[i]}\t{bt[i]}\t{ct[i]}\t{tat[i]}\t{wt[i]}")
+    
