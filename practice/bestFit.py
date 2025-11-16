54a4b0dcc51388204e819377233d1f7afa0d7731
Riad <riadk910@gmail.com>
2025-11-16 22:56:35 +0600

    bestFit

---
+++ practice/bestFit.py
@@ -0,0 +1,36 @@
+# Best Fit Memory Allocation Algorithm
+# Author: Beginner-friendly version
+
+# Input number of memory blocks
+n = int(input("Enter number of memory blocks: "))
+
+# Input sizes of each memory block
+blocks = list(map(int, input("Enter sizes of each block: ").split()))
+
+# Input number of processes
+m = int(input("Enter number of processes: "))
+
+# Input sizes of each process
+processes = list(map(int, input("Enter sizes of each process: ").split()))
+
+# Create allocation list
+allocation = [-1] * m
+
+# Best Fit Allocation
+for i in range(m):
+    best_index = -1
+    for j in range(n):
+        if blocks[j] >= processes[i]:
+            if best_index == -1 or blocks[j] < blocks[best_index]:
+                best_index = j
+    if best_index != -1:
+        allocation[i] = best_index
+        blocks[best_index] -= processes[i]
+
+# Print results
+print("\nProcess No.\tProcess Size\tBlock Allocated")
+for i in range(m):
+    if allocation[i] != -1:
+        print(f"P{i+1}\t\t{processes[i]}\t\tB{allocation[i]+1}")
+    else:
+        print(f"P{i+1}\t\t{processes[i]}\t\tNot Allocated")
